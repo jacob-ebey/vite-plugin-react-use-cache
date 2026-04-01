7557e7020ffedae64f0e0d6e511abb421c38b99b
Jacob Ebey <jacob.ebey@live.com>
2026-03-31 17:01:26 -0700

    add remix utils (#8)

--- packages/vite-plugin-react-use-cache/tsdown.config.ts
+++ packages/vite-plugin-react-use-cache/tsdown.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: [
-    "./src/runtime.ts",
-    "./src/unstorage.ts",
-    "./src/use-cache-plugin.ts",
-  ],
+  entry: {
+    remix: "./src/remix.ts",
+    runtime: "./src/runtime.ts",
+    unstorage: "./src/unstorage.ts",
+    "use-cache-plugin": "./src/use-cache-plugin.ts",
+  },
+  external: ["vite-plugin-react-use-cache/runtime"],
   dts: {
     tsgo: true,
   },
